583a6d23c925f82d25d64521d7e4a4d16550212e
mayaman26 <mayaman26@gmail.com>
2021-11-03 09:58:55 -0700

    trying to add font

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,11 @@ export default {
             { name: 'format-detection', content: 'telephone=no' }
         ],
         link: [
-            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+            { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+            { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: true },
+            { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Monsieur+La+Doulaise&display=swap', crossorigin: true }
+
         ],
         script: [{
             src: "https://code.jquery.com/jquery-3.5.1.min.js"
